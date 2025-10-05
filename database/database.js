42fb561df2906722569981ecaad78aecff412420
Arjun <arjunsah856@gmail.com>
2025-10-05 21:25:03 +0545

    made connection to database

---
+++ database/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const connectMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected sccessfully")
+    } catch (error) {
+        console.log("Failed to connect Database", error)
+    }
+}
