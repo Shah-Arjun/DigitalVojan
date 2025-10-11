04e74aadce057156a4ade91487e984c5dd7da4e7
Arjun <arjunsah856@gmail.com>
2025-10-12 04:49:50 +0545

    made create product api logic

--- controllers/admin/product/productController.js
+++ controllers/admin/product/productController.js
@@ -1,5 +1,25 @@
-exports.productController = (req, res) => {
-    const {productName, productDescription, productPrice, productStatus, productQuantity} = req.body
+const Product = require("../../../model/productModel")
 
+exports.createProduct = async (req, res) => {
+    const {productName, productDescription, productPrice, productStatus, productStockQty} = req.body
+
+    if(!productName || !productDescription || !productPrice || !productStatus || !productStockQty){
+        return res.status(400).json({
+            message: "Provide product name, description, price, status, StockQuantity"
+        })
+    }
+
+    // insert into product collection/table
+    await Product.create({
+        productName: productName,
+        productDescription: productDescription,
+        productPrice: productPrice,
+        productStatus: productStatus,
+        productStockQty: productStockQty
+    })
+    
+    res.status(200).json({
+        message: "Product created successfully"
+    })
     
 }
