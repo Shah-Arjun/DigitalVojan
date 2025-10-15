89f6ca00750283b74e6164f095e0e54c615681e5
Arjun <arjunsah856@gmail.com>
2025-10-15 13:02:39 +0545

    error handling for auth controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,16 @@
 const { registerUser, loginUser, forgetPassword, verifyOtp, resetPassword } = require('../controllers/auth/authController')
+const catchAsync = require('../services/catchAsync')
 
 
 //advance wy of maintaining routes
 const router = require('express').Router()
 
 //routes endpoints goes here
-router.route("/register").post(registerUser)     // when '/register' is hitted , registerUser post method invoked
-router.route("/login").post(loginUser)     // when '/login' is hitted , loginUser post method invoked
-router.route("/forgetPassword").post(forgetPassword)     // when '/forgetPassword' is hitted , forgetPassword post method invoked
-router.route("/verifyOtp").post(verifyOtp)
-router.route("/resetPassword").post(resetPassword)
+router.route("/register").post(catchAsync(registerUser))     // when '/register' is hitted , registerUser post method invoked
+router.route("/login").post(catchAsync(loginUser))     // when '/login' is hitted , loginUser post method invoked
+router.route("/forgetPassword").post(catchAsync(forgetPassword))     // when '/forgetPassword' is hitted , forgetPassword post method invoked
+router.route("/verifyOtp").post(catchAsync(verifyOtp))
+router.route("/resetPassword").post(catchAsync(resetPassword))
 
 
 module.exports = router
