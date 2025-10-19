7132dc7181b3737a37fbbd1251143979eff552ea
Arjun <arjunsah856@gmail.com>
2025-10-19 16:37:47 +0545

    fetched all users by admin except ownself

--- controllers/admin/user/userController.js
+++ controllers/admin/user/userController.js
@@ -3,7 +3,9 @@ const User = require("../../../model/userModel")
 
 // GET all users by admin
 exports.getUsers = async (req, res) => {
-    const users = await User.find()       // returns array of object
+    const loggedInAdminId = req.user.id   //form auth middleware
+    
+    const users = await User.find({_id: {$ne: loggedInAdminId}}).select(["+otp", "+isOtpVerified"])       // find() returns array of object
 
     if(users.length > 1) {           // user is not empty, 1 user is admin
         res.status(200).json({
--- routes/adminUsersRoutes.js
+++ routes/adminUsersRoutes.js
@@ -3,9 +3,10 @@
 const { getUsers } = require('../controllers/admin/user/userController')
 const isAuthenticated = require('../middleware/isAuthenticated')
 const restrictTo = require('../middleware/restrictTo')
+const catchAsync = require('../services/catchAsync')
 
 const router = require('express').Router()
 
-router.route('/users').get(isAuthenticated, restrictTo('admin'), getUsers)
+router.route('/users').get(isAuthenticated, restrictTo('admin'), catchAsync(getUsers))
 
 module.exports = router
