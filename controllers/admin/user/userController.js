1c47093531d73e5775a7cf1a60ad66616c7fc888
Arjun <arjunsah856@gmail.com>
2025-10-20 20:53:34 +0545

    delete user by admin api

--- controllers/admin/user/userController.js
+++ controllers/admin/user/userController.js
@@ -1,7 +1,7 @@
 const User = require("../../../model/userModel")
 
 
-// GET all users by admin
+// GET all users by admin api logic
 exports.getUsers = async (req, res) => {
     const loggedInAdminId = req.user.id   //form auth middleware
     
@@ -20,4 +20,29 @@ exports.getUsers = async (req, res) => {
     }
      
     
+}
+
+
+
+
+// DELETE user by admin api logic
+exports.deleteUser = async (req, res) => {
+    const userId = req.params.id
+    if(!userId) {
+        return res.status(400).json({
+            message: "Please provide user id"
+        })
+    }
+    //checks id the user exist or not 
+    const user = await User.findById(userId)
+    if(!user){
+        return res.status(404).json({
+            message: "No user found with that id"
+        })
+    }
+
+    await User.findByIdAndDelete(userId)
+    res.status(200).json({
+        message: "User deleted successfully"
+    })
 }
--- routes/adminUsersRoutes.js
+++ routes/adminUsersRoutes.js
@@ -1,12 +1,14 @@
-//function to access users by admin
+//route to access, delete users by admin
 
-const { getUsers } = require('../controllers/admin/user/userController')
+const { getUsers, deleteUser } = require('../controllers/admin/user/userController')
 const isAuthenticated = require('../middleware/isAuthenticated')
 const restrictTo = require('../middleware/restrictTo')
 const catchAsync = require('../services/catchAsync')
 
 const router = require('express').Router()
 
-router.route('/users').get(isAuthenticated, restrictTo('admin'), catchAsync(getUsers))
+router.route('/users').get(isAuthenticated, restrictTo('admin'), catchAsync(getUsers))  //get users
+
+router.route("/users/:id").delete(isAuthenticated, restrictTo('admin'), catchAsync(deleteUser))  //delete user
 
 module.exports = router
