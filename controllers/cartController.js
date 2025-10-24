b968eb89efa79204cc9013f76735aec7c8794b73
Arjun <arjunsah856@gmail.com>
2025-10-24 15:22:13 +0545

    get all cart items

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,8 @@
 const User = require('../model/userModel')
 const Product = require('../model/productModel')
 
+
+// ADD product to cart controller
 exports.addToCart = async (req, res) => {
     const userId = req.user.id
     const {productId} = req.params
@@ -20,5 +22,27 @@ exports.addToCart = async (req, res) => {
     user.save()
     res.status(200).json({
         message: "Product added to cart"
+    })
+}
+
+
+
+
+// GET all cart items based on user
+exports.getMyCartItems = async(req, res) => {
+    const userId = req.user.id
+    const userData = await User.findById(userId).populate({
+        path: "cart",                   // populate the cart column
+        select: "-productStatus -__v"         // don't fetch productStatus, __v
+    })   
+     if(userData.cart.length == 0){
+        return res.status(404).json({
+            message: "Cart is empty",
+            data: []
+        })
+    }
+    res.status(200).json({
+        message: "Cart data fetched successfully",
+        data: userData.cart
     })
 }
--- routes/user/cartRoutes.js
+++ routes/user/cartRoutes.js
@@ -1,9 +1,10 @@
-const { addToCart } = require('../../controllers/cartController')
+const { addToCart, getMyCartItems } = require('../../controllers/cartController')
 const isAuthenticated = require('../../middleware/isAuthenticated')
 const catchAsync = require('../../services/catchAsync')
 
 const router = require('express').Router()
 
+router.route('/').get(isAuthenticated, catchAsync(getMyCartItems))
 router.route('/:productId').post(isAuthenticated, catchAsync(addToCart))
 
 module.exports = router
