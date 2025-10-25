183ac144099269360dbd8d41345c18ab16ca9913
Arjun <arjunsah856@gmail.com>
2025-10-25 16:16:41 +0545

    getMyOrders by user contoller

--- controllers/user/order/orderController.js
+++ controllers/user/order/orderController.js
@@ -26,3 +26,15 @@ exports.createOrder = async (req, res) => {
 
 
 
+// GET MY ORDERS controller
+exports.getMyOrders = async (req, res) => {
+    const userId = req.user.id
+    const orders = await Order.find({user : userId}).populate({
+        path: 'items.product',
+        model: 'Product'
+    })
+    res.status(200).json({
+        message: "Product fetchend successfully",
+        data: orders
+    })
+} 
